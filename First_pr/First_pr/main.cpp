9e0c59428986d8016dc50747e07c0ee0fb838a4c
Igor Usenko <usenkoigor01@gmail.com>
2024-06-28 14:50:38 +0300

    Первое и второе занятие

---
+++ First_pr/First_pr/main.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	int age;
+	cout << "Your name:\n" << endl;
+	cin >> name;
+	cout << "Hello, " + name + "!" + " How old are you? Please write)\n" << endl;
+	cin >> age;
+	cout << "Good! So, your name: " + name + "and your age: " + to_string(age) << endl;
+	cout <<"Press any key to continue...";
+	cin.get();
+	return 0;
+}
---
+++ Lesson 2/Lesson 2/main.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "iostream"
+#include <ctime>
+
+
+using namespace std;
+
+int main()
+{
+	int ans, start, end, x, i=0;
+
+	while (true)
+	{
+		cout << "Try to guess the number! Enter your answer! Enther 0 - exit:" << endl;
+		cin >> ans;
+		start = 1;
+		end = 10;
+		x = rand() % (end - start + 1) + start;
+		if (ans == x) 
+		{
+			for (; ans <end; ans ++) 
+			{
+				cout << "Good! You win! Attempts: " << i << endl;
+				i = 0;
+			}
+		}
+		else if (ans == 0) 
+		{
+			return 0;
+		}
+		else 
+		{
+			cout << "Try again! The number was: "<< x << endl;
+			i = i + 1;
+		}
+	}
+}
